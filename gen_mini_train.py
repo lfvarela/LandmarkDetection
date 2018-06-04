6cc523579e64bdfe581c1ae8a7d0ea2082318af9
root <root@ld-vm-1.c.glass-genius-200423.internal>
2018-06-04 00:55:41 +0000

    changes to gen_mini_train.py

--- gen_mini_train.py
+++ gen_mini_train.py
@@ -3,13 +3,14 @@ import shutil
 import sys
 
 def gen_mini_train(data_dir):
-    for subdir in os.walk(data_dir):
+    dirs = [s[0] for s in os.walk(data_dir)]
+    for subdir in dirs:
         new_subdir = os.path.join('../mini-train/', subdir)
         os.makedirs(new_subdir)
         assert(os.path.isdir(new_subdir))
         for img in os.listdir(subdir)[:10]:
             if img.endswith(".jpg"):
-                shutil.copy(img, new_subdir)
+                shutil.copy(os.path.join(subdir, img), new_subdir)
 
 if __name__ == '__main__':
     directory = sys.argv[1]
